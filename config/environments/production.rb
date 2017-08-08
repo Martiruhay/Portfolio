5aaf19a54f7374c459475c41e51accc6126c16ba
Marti Lloveras <marti-nini@hotmail.com>
2017-08-08 21:13:09 +0000

    Add sendgrid to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.default_url_options = { host: "https://mitralesvoral.herokuapp.com/"}
+  config.action_mailer.delivery_method = :smtp
+  
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'mitralesvoral.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+  
 end
