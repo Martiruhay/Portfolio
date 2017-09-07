a09faebacde5d4321da2afbd80bd41b9c56d2653
Marti Lloveras <marti-nini@hotmail.com>
2017-09-07 10:18:07 +0000

    Change number of listed items in welcome screen

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
 class WelcomeController < ApplicationController
   
   def index
-    @posts = Post.all.limit(4).order("created_at desc")
-    @projects = Project.all.limit(4).order("created_at desc")
+    @posts = Post.all.limit(7).order("created_at desc")
+    @projects = Project.all.limit(3).order("created_at desc")
   end
   
 end
