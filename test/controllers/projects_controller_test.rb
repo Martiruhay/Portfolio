e9b75c872e1dd437db5409607137a8979139e983
Marti Lloveras <marti-nini@hotmail.com>
2017-10-29 01:44:39 +0200

    Test fixes

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -1,9 +1,5 @@
 require 'test_helper'
 
 class ProjectsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get projects_index_url
-    assert_response :success
-  end
 
 end
