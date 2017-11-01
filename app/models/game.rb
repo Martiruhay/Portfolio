1523cd0dc25987bcba54f99443fa9d3c02f9e9c0
Marti Lloveras <marti-nini@hotmail.com>
2017-11-01 02:00:13 +0100

    Ima break storage things

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,7 +4,7 @@ class Game < ApplicationRecord
   
   for i in 1..3 do
     has_attached_file "image#{i}", 
-      styles: { medium: "400x400>", small: "200x200>"}
+      styles: { big: "720x400>", medium: "400x400>", small: "200x200>"}
     validates_attachment_content_type "image#{i}", content_type: /\Aimage\/.*\z/
   end
   
