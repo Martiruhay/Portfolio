bdf7cb903e3c3b2c964f5dd919c340f86f4db8c8
Marti Lloveras <marti-nini@hotmail.com>
2017-11-01 02:15:10 +0100

    Hope I didn't brake it

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,7 +4,10 @@ class Game < ApplicationRecord
   
   for i in 1..3 do
     has_attached_file "image#{i}", 
-      styles: { big: "720x400>", medium: "400x400>", small: "200x200>"}
+      styles: { big: "720x400>", medium: "400x400>", small: "200x200>"},
+      path: Rails.env.development? ? 
+        "_dev/:class/:style/:slug/:attachment.:extension"
+        : ":class/:style/:slug/:attachment.:extension"
     validates_attachment_content_type "image#{i}", content_type: /\Aimage\/.*\z/
   end
   
