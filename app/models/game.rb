cc22d02d074587d1c95075e7ebf0b39488514457
Marti Lloveras <marti-nini@hotmail.com>
2017-11-01 15:39:50 +0100

    Wip image fix + game images bucket change

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,9 +5,7 @@ class Game < ApplicationRecord
   for i in 1..3 do
     has_attached_file "image#{i}", 
       styles: { big: "720x400>", medium: "400x400>", small: "200x200>"},
-      path: Rails.env.development? ? 
-        "_dev/:class/:style/:slug/:attachment.:extension"
-        : ":class/:style/:slug/:attachment.:extension"
+      path: ":class/:style/:slug/:attachment.:extension"
     validates_attachment_content_type "image#{i}", content_type: /\Aimage\/.*\z/
   end
   
